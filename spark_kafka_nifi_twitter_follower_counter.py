3647e4f3d4e0a2ebf26e628d502371c36cd34b97
euguroglu <e.uguroglu@yahoo.com>
2021-05-08 23:09:10 +0300

    from milisecond to second

--- spark_kafka_nifi_twitter_follower_counter.py
+++ spark_kafka_nifi_twitter_follower_counter.py
@@ -64,7 +64,7 @@ udfgetTeamTag = udf(lambda tag: getTeamTag(tag), StringType())
 explode_df = explode_df.withColumn("timestamp_ms", col("timestamp_ms").cast(LongType()))
 
 df = explode_df.select(
-    from_unixtime(col("timestamp_ms"),"yyyy-MM-dd HH:mm:ss").alias("timestamp"),
+    from_unixtime(col("timestamp_ms")/1000,"yyyy-MM-dd HH:mm:ss").alias("timestamp"),
     col("text"),
     col("id"),
     col("followers_count"),
